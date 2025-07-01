4b94225a0a3d60649bd80ab433668d17e27b392d
gimn70009 <risa1110@hansung.ac.kr>
2025-07-01 17:06:13 +0900

    feat: 정적 팩토리 메서드 구현

--- likelion/src/main/java/com/hansung/likelion/global/response/BaseResponse.java
+++ likelion/src/main/java/com/hansung/likelion/global/response/BaseResponse.java
@@ -1,4 +1,34 @@
 package com.hansung.likelion.global.response;
 
+import com.hansung.likelion.global.response.code.BaseResponseCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
 public class BaseResponse {
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+    private final String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+    // 정적 팩토리 메서드 패턴
+    public static BaseResponse of(Boolean isSuccess, BaseResponseCode baseResponseCode) {
+        return new BaseResponse(isSuccess, baseResponseCode.getCode(), baseResponseCode.getMessage());
+    }
+
+    // 정적 팩토리 메서드 패턴
+    public static BaseResponse of(Boolean isSuccess, BaseResponseCode baseResponseCode, String message) {
+        return new BaseResponse(isSuccess, baseResponseCode.getCode(), message);
+    }
+
+    // 정적 팩토리 메서드 패턴
+    public static BaseResponse of(Boolean isSuccess, String code, String message) {
+        return new BaseResponse(isSuccess, code, message);
+    }
 }
